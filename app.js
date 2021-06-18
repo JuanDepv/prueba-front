ecb0d7603a337b95516029989d552403f28d10d2
Juan Jose Restrepo Cuadros <cuadrosc99@gmail.com>
2021-06-17 20:31:11 -0500

    agregando validacion de fecha dia

--- app.js
+++ app.js
@@ -1,143 +1,41 @@
 const URL_BASE = "http://test.movilbox.co:888/test_mbox/test.php?metodo=";
-const calendar = document.querySelector("#calendar");
-let draggableUser = document.querySelector("#usuarios");
+let calendarEl = "";
+let draggableUser ="";
+let Draggable = "";
+
+let menu = document.querySelector("#context-menu");
+let menuItems = menu.querySelectorAll(".context-menu__item");
+let menuState = 0;
+
 var dataUser = [];
 var dataPeriods = [];
+
 const headers = {'Content-Type': 'application/json'};
 
-getDataUser();
-getdataPeriods();
-
-// getDataUser()
-//   .then((resp) => resp.json())
-//   .then((data) => (this.dataUser = data));
-
-// getdataPeriods()
-//   .then((resp) => resp.json())
-//   .then((data) => (this.dataPeriods = data));
 
 document.addEventListener("DOMContentLoaded", function () {
-  
-  setTimeout(() => {
-    getUser();
+    getDataUser();
+    getdataPeriods();
+    setTimeout(() => {
+        InitCalendar();
+        $(".fc-daygrid-day-events").click(function (e) {
+            positionMenu(e);
+        });
 
-    new FullCalendar.Draggable(draggableUser, {
-      itemSelector: "#user",
-      eventData: function (eventEl) {
-        return {
-          title: eventEl.getAttribute("data-name"),
-        };
-      },
-    });
-
-    const calendarLib = new FullCalendar.Calendar(calendar, {
-      initialView: "dayGridMonth",
-      themeSystem: "bootstrap",
-      timeZone: "UTC",
-      locale: "es",
-
-      // date Navigation - initial perids and range Periods
-      initialDate: "2020-06-01",
-      validRange: {
-        start: firstPositionDate().toString(),
-        end: lastPositionDate().toString(),
-      },
-      headerToolbar: {
-        left: "prev,next",
-        center: "title",
-        right: "dayGridMonth",
-      },
-
-      
-      // test schedules - default
-      events: {
-        url: "events.json",
-        success: function (response) {
-        },
-        failure: function (response) {
-        },
-        eventDataTransform: function (event) {
-          
-        },
-      },
-
-      //sizing
-      height: 700,
-
-      // date nav links
-      navLinks: true,
-
-      //
-      fixedWeekCount: false,
-
-      // date click and selecting
-      selectable: true,
-      selectMirror: true,
-
-      // events popover
-      dayMaxEventRows: true,
-      views: {
-        timeGrid: {
-          dayMaxEventRows: 4, // adjust to 6 only for timeGridWeek/timeGridDay
-        },
-      },
-      // select: function (info) {
-      //   // calendar.addEvent({
-      //   //   title: title,
-      //   //   start: arg.start,
-      //   //   end: info.startStr,
-      //   //   allDay: info.endStr,
-      //   // });
-      //   alert(info);
-      //   alert("selected " + info.startStr + " to " + info.endStr);
-      // },
-
-      eventClick: function(info) {
-
-        // alert('Event: ' + info.event.title);
-        // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-        // alert('View: ' + info.view.type);
-      },
-
-      eventOverlap: function (stillEvent, movingEvent) {
-        return stillEvent.allDay && movingEvent.allDay;
-      },
-
-      editable: true,
-      droppable: true,
-      eventResizableFromStart: true,
-      drop: function (info) {
-        let id = info.draggedEl.attributes[2].value;
-        let nombreProgramado = info.draggedEl.attributes[3].value;
-        let fecha = info.dateStr;
-
-        setLocalStorage(id, nombreProgramado, fecha, fecha);
-      },
-
-      eventReceive: function (info) {
-        // eventDay(info.event.startStr);
-        // alert("event received! - eventReceive", info.event);
-      },
-
-      // confirm for move the event
-      eventDrop: function (info) {
-        if (!confirm("Estas seguro de cambiar la programación")) {
-          info.revert();
-        }
-      },
-    });
-    
-    calendarLib.render();
-    
-  }, 2000);
-
-});
+        $(document).click(function (e) {
+            if (e.target.classList.contains('fc-event-title') === true) {
+                toggleMenuOn();
+            } else {
+                toggleMenuOff();
+            }
+        });
+    }, 1000);
+})
 
 /**
  * events -
  * @author Juan Jose
  */
-
 
 document
   .querySelector("#buscador")
@@ -158,32 +56,32 @@ document
   });
 
 document
-  .querySelector("#save-programation")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    let data = getLocalStorage();
+    .querySelector("#save-programation")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        let data = getLocalStorage();
 
-    if (data != undefined) {
-      if(data.length > 0) {
-        fetch(`${URL_BASE}guardar`, {
-          method: "POST",
-          body: data,
-        })
-          .then((data) => data.json())
-          .then((res) => {
-            console.log(res);
-            if (res.state === 1) {
-              alert("se ha enviado los datos");
-              localStorage.removeItem("programation");
-              localStorage.setItem("programation", "[]");
+        if (data != undefined) {
+            if(data.length > 0) {
+                fetch(`${URL_BASE}guardar`, {
+                    method: "POST",
+                    body: data,
+                })
+                .then((data) => data.json())
+                .then((res) => {
+                    console.log(res);
+                    if (res.state === 1) {
+                        alert("se ha enviado los datos");
+                        localStorage.removeItem("programation");
+                        localStorage.setItem("programation", "[]");
+                    }
+                });
+            } else {
+                alert("No hay pramaciones para enviar....!")  
             }
-          });
-      } else {
-        alert("No hay pramaciones para enviar....!")  
-      }
-    } else {
-      alert("No hay pramaciones para enviar....!")
-    }
+        } else {
+            alert("No hay pramaciones para enviar....!")
+        }
   });
 
 
@@ -193,15 +91,160 @@ document
  * @author Juan Jose
  */
 
+ function InitCalendar(){
+    getUser();
+
+    calendarEl = document.querySelector("#calendar");
+    draggableUser = document.querySelector("#usuarios");
+
+    Draggable = new FullCalendar.Draggable(draggableUser, {
+        itemSelector: "#user",
+        eventData: function (eventEl) {
+            return {
+                id: eventEl.getAttribute("data-name"),
+                title: eventEl.innerText,
+                color: getRandomColor()
+            };
+        },
+    });
+
+    calendar = new FullCalendar.Calendar(calendarEl, {
+        locale: "es",
+        timeZone: "UTC",
+        initialView: "dayGridMonth",
+        initialDate: "2020-06-01",
+
+        //toolbar
+        headerToolbar: {
+            left: "prev,next",
+            center: "title",
+            right: "dayGridMonth",
+        },
+
+        // validacion de fechas
+        validRange: {
+            start: firstPositionDate().toString(),
+            end: lastPositionDate().toString(),
+        },
+
+        // sizing
+        height: 700,
+
+        // date nav links
+        navLinks: true,
+
+        //
+        fixedWeekCount: false,
+
+        // date click and selecting
+        selectable: true,
+        selectMirror: true,
+
+        // events popover
+        dayMaxEventRows: true,
+        views: {
+            timeGrid: {
+                dayMaxEventRows: 4, // adjust to 6 only for timeGridWeek/timeGridDay
+            },
+        },
+
+        editable: true,
+        dayMaxEvents: true,
+        droppable: true,
+
+        drop: function (event) {
+            let id = event.draggedEl.attributes[2].value;
+            let nombreProgramado = event.draggedEl.attributes[3].value;
+            let idEventDropped = event.draggedEl.getAttribute("data-name");
+            let fecha = event.dateStr;
+
+            console.log(calendar.getEvents());
+
+            if ((calendar.getEvents().filter(e => e.id === idEventDropped && e.startStr === fecha).length > 0) === true) {
+                calendar.getEvents().filter(e => e.id === idEventDropped && e.startStr === fecha)[0].remove();
+                alert('Este usuario ya se encuentra programado para este día !');
+            } else {
+                setLocalStorage(id, nombreProgramado, fecha, fecha);
+            }
+        },
+
+        // confirm for move the event
+        eventDrop: function (event) {
+            if (!confirm("Estas seguro de cambiar la programación")) {
+                event.revert();
+            }
+        },
+
+    });
+
+    calendar.render();
+}
+
+ function toggleMenuOn() {
+    if (menuState !== 1) {
+        menuState = 1;
+        menu.classList.add("context-menu--active");
+    }
+}
+
+function toggleMenuOff() {
+    if (menuState !== 0) {
+        menuState = 0;
+        menu.classList.remove("context-menu--active");
+    }
+}
+
+ function positionMenu(e) {
+
+    clickCoords = getPosition(e);
+    clickCoordsX = clickCoords.x;
+    clickCoordsY = clickCoords.y;
+    menuWidth = menu.offsetWidth + 4;
+    menuHeight = menu.offsetHeight + 4;
+
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+
+    if ((windowWidth - clickCoordsX) < menuWidth) {
+        menu.style.left = windowWidth - menuWidth + "px";
+    } else {
+        menu.style.left = clickCoordsX + "px";
+    }
+
+    if ((windowHeight - clickCoordsY) < menuHeight) {
+        menu.style.top = windowHeight - menuHeight + "px";
+    } else {
+        menu.style.top = clickCoordsY + "px";
+    }
+}
+
+function getPosition(e) {
+    var posx = 0;
+    var posy = 0;
+
+    if (!e) var e = window.event;
+
+    if (e.pageX || e.pageY) {
+        posx = e.pageX;
+        posy = e.pageY;
+    } else if (e.clientX || e.clientY) {
+        posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    return {
+        x: posx,
+        y: posy
+    }
+}
+
 // returns the info users
 function getDataUser() {
-  // fetch(`${URL_BASE}usuarios`, headers);
-  // return response;
 
   fetch(`${URL_BASE}usuarios`, headers)
-    .then((resp) => resp.json())
-    .then((data) => (this.dataUser = data))
-    .catch((error) => console.log(error));
+        .then((resp) => resp.json())
+        .then((data) => (this.dataUser = data))
+        .catch((error) => console.log(error));
 }
 
 // returns the periods
@@ -210,26 +253,26 @@ function getdataPeriods() {
   // return response;
 
   fetch(`${URL_BASE}periodos`, headers)
-    .then((resp) => resp.json())
-    .then((data) => (this.dataPeriods = data))
-    .catch((error) => console.log(error));
+        .then((resp) => resp.json())
+        .then((data) => (this.dataPeriods = data))
+        .catch((error) => console.log(error));
 }
 
 // genarete a random color
 function getRandomColor() {
-  let r = Math.floor(Math.random() * 256);
-  let g = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-  let color = `rgb(${r}, ${g}, ${b})`;
-  return color;
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    return color;
 }
 
 // return a currentDate
 function getCurrentDate() {
-  let date = new Date();
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
 
   if (day < 10) {
     day = `0${day}`;
@@ -288,40 +331,41 @@ function templeteUser(response) {
 
 // date periods init
 function firstPositionDate() {
-  const last = this.dataPeriods.shift();
-  let endDate = "" + last.anio + "-0" + last.mes + "-01";
-  return endDate;
+    const last = this.dataPeriods.shift();
+    let endDate = "" + last.anio + "-0" + last.mes + "-01";
+    return endDate;
 }
 
 // date periods final
 function lastPositionDate() {
-  const last = this.dataPeriods.slice(-1);
-  let endDate = "" + last[0].anio + "-" + last[0].mes + "-01";
-  return endDate;
+    const last = this.dataPeriods.slice(-1);
+    let endDate = "" + last[0].anio + "-" + last[0].mes + "-01";
+    return endDate;
 }
 
 // localstorage
 function setLocalStorage(id, title, fecha_init, fecha_end) {
-  let old_data = [];
+    let old_data = [];
 
-  if (localStorage.getItem("programation") == null) {
-    localStorage.setItem("programation", "[]");
-  }
+    if (localStorage.getItem("programation") == null) {
+        localStorage.setItem("programation", "[]");
+    }
 
-  const object = {
-    idus: id,
-    fecha: fecha_init,
-  };
+    const object = {
+        idus: id,
+        fecha: fecha_init,
+    };
 
-  old_data = JSON.parse(localStorage.getItem("programation"));
-  old_data.push(object);
-
-  localStorage.setItem("programation", JSON.stringify(old_data));
+    old_data = JSON.parse(localStorage.getItem("programation"));
+    old_data.push(object);
+    localStorage.setItem("programation", JSON.stringify(old_data));
 }
 
 function getLocalStorage() {
-  const storage = localStorage;
-  if (storage.getItem("programation") != null) {
-    return JSON.parse(storage.getItem("programation"));
-  }
+    const storage = localStorage;
+    if (storage.getItem("programation") != null) {
+        return JSON.parse(storage.getItem("programation"));
+    }
 }
+
+
